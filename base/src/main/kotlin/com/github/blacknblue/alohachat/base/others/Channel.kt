c6074afa75ffdb9c8003ba26538dc50a57bb8276
OtakuSenpai <edu.avraneel@gmail.com>
2019-03-12 22:03:58 +0000

    Made some changes to the gradle build files. Added some more things in Channel class

--- base/src/main/kotlin/com/github/blacknblue/alohachat/base/others/Channel.kt
+++ base/src/main/kotlin/com/github/blacknblue/alohachat/base/others/Channel.kt
@@ -3,7 +3,14 @@ package com.github.blacknblue.alohachat.base.others
 import com.github.blacknblue.alohachat.base.helpers.KtQueue
 import com.github.blacknblue.alohachat.base.message_parse.Prefix
 
-abstract class Channel(ownPrefix: Prefix) {
+abstract class Channel() {
+
+    constructor(ownPrefix: Prefix) : this() {
+        senderPrefix = ownPrefix
+    }
+
+    fun matchChannels(other: Channel): Boolean = (this.senderPrefix == other.senderPrefix)
+
     var messageList = KtQueue()
-    var senderPrefix = ownPrefix
+    var senderPrefix = Prefix()
 }
