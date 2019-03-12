d9a262fdf402dec27bc28a8503033767fad58743
OtakuSenpai <Find me at Freenode>
2019-03-12 22:03:58 +0000

    Made some changes to the gradle build files. Added some more things in Channel class

---
+++ server/src/main/kotlin/AlohaServ.kt
@@ -0,0 +1,21 @@
+import com.github.blacknblue.alohachat.base.others.Channel
+
+class AlohaServ(servName: String, servIP: String, servPort: String) {
+
+    fun init() {
+
+    }
+
+
+    fun run() {
+
+
+    }
+
+
+
+    var channelList = MutableList<Channel>(0) {}
+    var serverPort = servPort
+    var serverIP = servIP
+    var serverName: String = servName
+}
